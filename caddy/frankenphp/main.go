6d14d16f6a15b5ad9eb669e40f34f3e17a763cb0
tobfel <131913095+tobfel@users.noreply.github.com>
2026-03-15 17:37:25 +0100

    Add new Caddy modules to main.go
    Signed-off-by: tobfel <131913095+tobfel@users.noreply.github.com>

--- caddy/frankenphp/main.go
+++ caddy/frankenphp/main.go
@@ -4,10 +4,27 @@ import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
 	// plug in Caddy modules here.
+	_ "github.com/abiosoft/caddy-exec"
+	_ "github.com/baldinof/caddy-supervisor"
+	_ "github.com/caddy-dns/desec"
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
+	_ "github.com/caddyserver/replace-response"
+	_ "github.com/caddyserver/transform-encoder"
+	_ "github.com/corazawaf/coraza-caddy/v2"
+	_ "github.com/dunglas/caddy-cbrotli"
 	_ "github.com/dunglas/frankenphp/caddy"
 	_ "github.com/dunglas/mercure/caddy"
 	_ "github.com/dunglas/vulcain/caddy"
+	_ "github.com/greenpau/caddy-security"
+	_ "github.com/hslatman/caddy-crowdsec-bouncer"
+	_ "github.com/hslatman/caddy-crowdsec-bouncer/appsec"
+	_ "github.com/hslatman/caddy-crowdsec-bouncer/http"
+	_ "github.com/hslatman/caddy-crowdsec-bouncer/layer4"
+	_ "github.com/mholt/caddy-dynamicdns"
+	_ "github.com/mholt/caddy-events-exec"
+	_ "github.com/mholt/caddy-webdav"
+	_ "github.com/porech/caddy-maxmind-geolocation"
+	_ "github.com/sagikazarmark/caddy-fs-s3"
 )
 
 func main() {
